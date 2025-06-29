6e4b17eb5f5d18d48a0d7250a1200feadf8ced5b
Hilda Abiyyu Rahmadani <danirahmadani91@gmail.com>
2025-06-29 13:46:19 +0700

    add homepage

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Redirect root to login
 Route::get('/', function () {
-    return redirect('/login');
+    return view('welcome');
 });
+
 
 // Guest routes (only accessible when not authenticated)
 Route::middleware('guest')->group(function () {
